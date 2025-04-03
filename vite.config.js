80c4f88e01675f0e26952884945fee2db2f4e1b4
Rixchiolina <recchiafabio04@gmail.com>
2025-04-03 11:28:59 +0200

    chore: update base path and add gh-pages deployment support
    Updated the base path in vite.config.js to match the GitHub repository name and added gh-pages as a dependency to enable deployment to GitHub Pages. Also, included the homepage and deploy script in package.json to streamline the deployment process.

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'; // Changed from @vitejs/plugin-rea
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/portfolio/', // Change this to your GitHub repository name
+  base: '/Portfolio/', // Change this to your GitHub repository name
 });
